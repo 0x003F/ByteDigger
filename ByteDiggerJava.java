86ac0afb384735cfc9aa470e34ef26bb4fca2e19
0x003F <166497524+0x003F@users.noreply.github.com>
2024-04-10 21:14:16 +0200

    Update ByteDiggerJava.java

--- ByteDiggerJava.java
+++ ByteDiggerJava.java
@@ -7,18 +7,18 @@ public class ByteDiggerJava
     public static void main(String[] args) throws Exception
     {
         final int MAIN_DIRECTORY_PATH_LENGTH=3;
-        int scelta=0;
+        int input=-1;
 
         System.out.println("1) Analyze the current mass storage.");
         System.out.println("2) Add a malicious file to your blacklist.");
         System.out.println("3) Exit.");
 
-        while(scelta!=3)
+        while(input!=3)
         {
             Scanner scanner=new Scanner(System.in);
-            scelta=scanner.nextInt();
+            input=scanner.nextInt();
 
-            if(scelta==1)
+            if(input==1)
             {
                 System.out.println("Please wait...");
                 File rootDirectory=new File(Paths.get("").toAbsolutePath().toString().substring(0, MAIN_DIRECTORY_PATH_LENGTH));
@@ -27,7 +27,7 @@ public class ByteDiggerJava
                 System.out.println(finalMatchingHashes+" malicious files has been found inside the current mass storage: "+rootDirectory.getAbsolutePath());
             }
 
-            if(scelta==2)
+            if(input==2)
             {
                 System.out.println("Write the path of your malicious file: ");
 
